c40e4e29200c773c5a3d3d09eb86dee5d8ab38c5
rosk3r <logthelog@gmail.com>
2024-04-14 12:55:49 +0600

    delete unused import

--- app/src/main/java/ru/rosk3r/composetest/presentation/components/MyNavigationbar.kt
+++ app/src/main/java/ru/rosk3r/composetest/presentation/components/MyNavigationbar.kt
@@ -1,8 +1,13 @@
 package ru.rosk3r.composetest.presentation.components
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.Icon
