c2f33bde98cb8c034f113b303c8e742f4059f1c1
rosk3r <logthelog@gmail.com>
2024-04-15 10:58:45 +0600

    add room database

--- app/src/main/java/ru/rosk3r/composetest/MainActivity.kt
+++ app/src/main/java/ru/rosk3r/composetest/MainActivity.kt
@@ -7,16 +7,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.room.Room
 import ru.rosk3r.composetest.presentation.view.ArchiveScreen
 import ru.rosk3r.composetest.presentation.view.CalendarScreen
 import ru.rosk3r.composetest.presentation.view.GoalScreen
 import ru.rosk3r.composetest.presentation.view.SettingsScreen
 import ru.rosk3r.composetest.presentation.view.ToDoScreen
+import ru.rosk3r.composetest.util.GoalGetterDatabase
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -44,29 +48,42 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MyApp(startDestination: String) {
     val navController = rememberNavController()
+    val database = provideDatabase()
 
     NavHost(navController = navController, startDestination) {
         composable("signUp") {
-            SignUpScreen(navController)
+            SignUpScreen(navController, database)
         }
         composable("signIn") {
             SignInScreen(navController)
         }
         composable("screen_1") {
-            ToDoScreen(navController = navController)
+            ToDoScreen(navController)
         }
         composable("screen_2") {
-            ArchiveScreen(navController = navController)
+            ArchiveScreen(navController)
         }
         composable("screen_3") {
-            CalendarScreen(navController = navController)
+            CalendarScreen(navController)
         }
         composable("screen_4") {
-            GoalScreen(navController = navController)
+            GoalScreen(navController)
         }
         composable("screen_5") {
-            SettingsScreen(navController = navController)
+            SettingsScreen(navController)
         }
+    }
+}
+
+@Composable
+fun provideDatabase(): GoalGetterDatabase {
+    val context = LocalContext.current
+    return remember {
+        Room.databaseBuilder(
+            context.applicationContext,
+            GoalGetterDatabase::class.java,
+            "app_database"
+        ).build()
     }
 }
 
