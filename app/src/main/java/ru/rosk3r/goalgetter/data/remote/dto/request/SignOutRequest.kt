4b1637756adb20ae1d93bd61f82530cec30549f4
rosk3r <logthelog@gmail.com>
2024-04-28 17:53:59 +0600

    add sign out

---
+++ app/src/main/java/ru/rosk3r/goalgetter/data/remote/dto/request/SignOutRequest.kt
@@ -0,0 +1,53 @@
+package ru.rosk3r.goalgetter.data.remote.dto.request
+
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody
+import okhttp3.RequestBody.Companion.toRequestBody
+import okhttp3.Response
+import org.json.JSONObject
+import java.io.IOException
+import java.util.concurrent.CountDownLatch
+
+class SignOutRequest(
+    private val token: String,
+) {
+    fun request(signOutRequest: SignOutRequest): Boolean {
+        val okHttpClient = OkHttpClient()
+
+        val json = JSONObject()
+
+        val requestBody: RequestBody = json.toString()
+            .toRequestBody("application/json; charset=utf-8".toMediaType())
+
+        val request = Request.Builder()
+            .delete(requestBody)
+            .addHeader("Content-Type", "application/json")
+            .addHeader("token", token)
+            .url("https://pumped-tough-sunbeam.ngrok-free.app/sign-out")
+            .build()
+
+        val latch = CountDownLatch(1)
+
+        okHttpClient.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                e.printStackTrace()
+                latch.countDown()
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                if (!response.isSuccessful) {
+                    println("Response was not successful: ${response.code}")
+                    latch.countDown()
+                    return
+                }
+            }
+        })
+        latch.await()
+
+        return true
+    }
+}
